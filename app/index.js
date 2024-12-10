9ec8ab1052cee0e02124f7b282a5d253e2d687a5
Aluno <diogo.bortoto88@gmail.com>
2024-12-10 20:00:57 -0300

    Commit

--- app/index.js
+++ app/index.js
@@ -1,204 +1,79 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, Pressable } from 'react-native';
-import { TaskCard } from './TaskCard';
-import { useEffect, useState } from 'react'; 
-import { BookCard } from './BookCard';
+import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function Page() {
-  const exampel = [{id: 1},{id: 2},{id: 3},{id:4}]
+export default function PAGE() {
+  const router = useRouter();
 
-  return(
-    <View style={styles.container}>
-    <Text>Open up Aoo.js to start working on your app!</Text>
-    <StatusBar style="auto" />
-
-  {example.map((item, index)=>{
-    <Pressable onPress={() => {}}>
-      <Text>go to page book </Text>
-    </Pressable>
-
-  })}
-
-    </View>
-
-
-);
-}
- 
-  const [taskTitle, setTaskTitle] = useState("");
-  const [taskDescripition, setTaskDescripition] = useState("");
-  const [task, setTask] = useState([]);
-  const [alert1, setAlert1] = useState(false);
-  const [alert2, setAlert2] = useState(false);
-
-
-  const onMessage = () => {
-    setAlert1(false);
-    setAlert2(false);
-
-    if (taskTitle !== "" && taskDescripition.length >= 10) {
-      setTask([
-        ...task,
-        {
-          id: task.length + 1,
-          title: taskTitle(""),
-          description: taskDescripition("")
-        }
-      ]
-
-      )
-
-
-
-    } else {
-
-      if (taskDescripition.length < 10) {
-
-        setAlert1(true)
-        setAlert2(() => {
-          setAlert1(false);
-        }, 4000);
-
-      }
-
-    }
-  }
-  const deleteTask = (index) => {
-    const updateTask = [...task];
-    updateTask.splice(index, 1)
-    setTask(updateTask);
   
-  };
+  const data = [
+    { id: '1', title: 'Dom Casmurro', author: 'Machado de Assis', year: '1899', qtd: '2' },
+    { id: '2', title: 'Memórias Póstumas de Brás Cubas', author: 'Machado de Assis', year: '1881', qtd: '3' },
+    { id: '3', title: 'Grande Sertão: Veredas', author: 'João Guimarães Rosa', year: '1956', qtd: '4' },
+    { id: '4', title: 'O Cortiço', author: 'Aluísio Azevedo', year: '1890', qtd: '4' },
+    { id: '5', title: 'Iracema', author: 'José de Alencar', year: '1865', qtd: '1' },
+    { id: '6', title: 'Macunaíma', author: 'Mário de Andrade', year: '1928', qtd: '11' },
+    { id: '7', title: 'Capitães da Areia', author: 'Jorge Amado', year: '1937', qtd: '2' },
+    { id: '8', title: 'Vidas Secas', author: 'Graciliano Ramos', year: '1938', qtd: '9' },
+    { id: '9', title: 'A Moreninha', author: 'Joaquim Manuel de Macedo', year: '1844', qtd: '2' },
+    { id: '10', title: 'O Tempo e o Vento', author: 'Erico Verissimo', year: '1949', qtd: '1' },
+    { id: '11', title: 'A Hora da Estrela', author: 'Clarice Lispector', year: '1977', qtd: '1' },
+    { id: '12', title: 'O Quinze', author: 'Rachel de Queiroz', year: '1930', qtd: '1' },
+    { id: '13', title: 'Menino de Engenho', author: 'José Lins do Rego', year: '1932', qtd: '5' },
+    { id: '14', title: 'Sagarana', author: 'João Guimarães Rosa', year: '1946', qtd: '3' },
+    { id: '15', title: 'Fogo Morto', author: 'José Lins do Rego', year: '1943', qtd: '1' },
+  ];
 
-  useEffect (() => {
-    const fetchData = async () => {
-      try {
-        const resp = await getRequest();
-        setTask(resp)
-      } catch (ex) {
-        console.error(ex)
-
-      }
-    } 
-
-    fetchData()
-  })
-  
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
 
-      <Text style={styles.label}>App de Tarefas</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome da tarefa:"
-        value={taskTitle}
-        onChangeText={setTaskTitle}
+      {/* Título */}
+      <Text style={styles.title}>Alexandria</Text>
+
+      {/* Lista de Livros */}
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => {
+              router.push(`/books/${item.id}`);
+            }}
+            style={styles.pressableButton}
+          >
+            <Text style={styles.pressableText}>{item.title}</Text>
+          </Pressable>
+        )}
       />
-
-
-
-      {alert1
-        ?
-        <text style={styles.errorText}>
-          Necessário informar o título
-        </text>
-        : <></>}
-
-
-
-      <Text>Tarefa Descrição:</Text>
-      <TextInput
-        style={[styles.input, styles.textArea]}
-        placeholder='Descrição da tarefa:'
-        multiline
-        value={taskDescripition}
-        onChangeText={setTaskDescripition}
-      />
-
-      {alert2
-        ?
-        <text style={styles.errorText}
-        >Necessário mínimo 10 caracteres
-        </text>
-        : <></>
-      }
-
-      <View style={styles.buttonContainer}>
-        <Button style={styles.buttonElement}
-          color="darkgreen"
-          title='Salvar'  
-          onPress={() =>
-            onMessage()} />
-      </View>
-
-      {
-        task.length > 0 ? <view style={styles.separetor} />
-          : <></>
-      }
-
-      <ScrollView>
-        {
-          task.map((item, index) => (
-            <BookCard
-              title={item.title}
-              descripition={item.descripition}
-
-              status={"Done"}
-              onClick={() => {
-                deleteTask(index);
-              }}
-            />
-          ))}
-      </ScrollView>
     </View>
-
   );
-
-
-
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 16,
-    backgroundColor: '#f4f4f4',
+    backgroundColor: '#F8F8F8',
+    paddingTop: 50,
+    paddingHorizontal: 20,
   },
-  label: {
-    fontSize: 24,
+  title: {
+    fontSize: 32,
     fontWeight: 'bold',
-    marginVertical: 20,
     color: '#333',
+    marginBottom: 20,
+    textAlign: 'center',
   },
-  input: {
-    width: '100%',
-    height: 40,
-    paddingHorizontal: 8,
-    borderWidth: 1,
-    borderRadius: 4,
-    borderColor: '#ccc',
-    marginVertical: 10,
+  pressableButton: {
+    backgroundColor: '#008B8B', 
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignItems: 'center',
   },
-  textArea: {
-    height: 100,
-    textAlignVertical: 'top'
+  pressableText: {
+    fontSize: 16,
+    color: '#FFF',
+    fontWeight: 'bold',
   },
-  buttonContainer: {
-    marginTop: 16,
-  },
-  buttonElement: {
-    borderRadius: 12
-  },
-  separetor: {
-    marginTop: 16,
-    width: "100%",
-    height: 1,
-    backgroundColor: "#222"
-  },
-  errorText: {
-    color: "red",
-    fontSize: 12,
-    frontStyle: "italic"
-  }
-
 });
